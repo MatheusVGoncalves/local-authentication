959c7032bfe8d2bbbc5dc72a23013912899e5309
MatheusVGoncalves <math_negocios@hotmail.com>
2023-01-18 20:55:10 -0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as LocalAuthentication from "expo-local-authentication";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function authenticate() {
+    const result = await LocalAuthentication.authenticateAsync();
+    setIsAuthenticated(result.success);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {isAuthenticated ? (
+        <Text> Você está autenticado</Text>
+      ) : (
+        <>
+          <Text> Você não está autenticado</Text>
+          <TouchableOpacity onPress={authenticate} style={styles.button}>
+            <Text>Autenticar</Text>
+          </TouchableOpacity>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "green",
+    borderRadius: 5,
+    marginTop: 15,
   },
 });
